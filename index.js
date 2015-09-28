bcacb99d53f197ab4f95eb87c18e941f19e88692
Shane Tomlinson <stomlinson@mozilla.com>
2015-09-28 10:25:56 +0100

    feat: Add `ignoreObjectsWithMethods`
    Add the ability to ignore any object with methods.

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ module.exports = {
         "sort-object-props": function(context) {
             var caseSensitive = context.options[0].caseSensitive;
             var ignoreMethods = context.options[0].ignoreMethods;
+            var ignoreObjectsWithMethods = context.options[0].ignoreObjectsWithMethods;
             var ignorePrivate = context.options[0].ignorePrivate;
 
             var MSG = "Property names in object literals should be sorted";
             return {
                 "ObjectExpression": function(node) {
+                    if (node.properties.length < 2) {
+                      return;
+                    }
+
+                    if (ignoreObjectsWithMethods) {
+                      var functionExpressions = node.properties.filter(function (property) {
+                        return property.value.type === "FunctionExpression";
+                      });
+
+                      // Objects with function expressions are not sorted, they may be prototypes
+                      if (functionExpressions.length) {
+                        return;
+                      }
+                    }
+
                     node.properties.reduce(function(lastProp, prop) {
                         if (ignoreMethods &&
                                 prop.value.type === "FunctionExpression") {
@@ -42,6 +58,6 @@ module.exports = {
         }
     },
     rulesConfig: {
-        "sort-object-props": [ 1, { caseSensitive: false, ignoreMethods: false } ]
+        "sort-object-props": [ 1, { caseSensitive: false, ignoreMethods: false, ignoreObjectsWithMethods: false, ignorePrivate: false } ]
     }
 };
